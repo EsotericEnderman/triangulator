1bce0b5a72f844cf3498512b3c2836da20c4374a
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-07-08 19:38:57 +0100

    Add utility class to check if vectors are linearly dependent

---
+++ src/main/java/net/slqmy/triangulator/util/VectorUtil.java
@@ -0,0 +1,22 @@
+package net.slqmy.triangulator.util;
+
+import org.joml.Vector2d;
+
+public class VectorUtil {
+    public static boolean areLinearlyDependent(Vector2d vectorA, Vector2d vectorB) {
+        double x1 = vectorA.x;
+        double y1 = vectorA.y;
+
+        double x2 = vectorB.x;
+        double y2 = vectorB.y;
+
+        if (x1 == 0 && y1 == 0) {
+            return x2 == 0 && y2 == 0;
+        }
+
+        double quotient1 = x2/x1;
+        double quotient2 = y2/y1;
+
+        return quotient1 == quotient2 || (x1 == 0 && x2 == 0) || (y1 == 0 && y2 == 0);
+    }
+}
